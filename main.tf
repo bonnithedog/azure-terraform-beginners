d715efa8fd9f4627c7a6e74ece486a0d259b7fb4
bonnithedog <45508928+bonnithedog@users.noreply.github.com>
2020-07-25 07:19:24 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -177,28 +177,28 @@ resource "azurerm_virtual_machine" "site" {
 }
 
 ##############################################################################
- * Azure MySQL Database
- Terraform can build any type of infrastructure, not just virtual machines. 
- Azure offers managed MySQL database servers and a whole host of other 
- resources. Each resource is documented with all the available settings:
- https://www.terraform.io/docs/providers/azurerm/r/mysql_server.html
- Uncomment the code below to add a MySQL server to your resource group.
- resource "azurerm_mysql_server" "mysql" {
-   name                = var.mysql_hostname
-   location            = azurerm_resource_group.tf_azure_guide.location
-   resource_group_name = azurerm_resource_group.tf_azure_guide.name
-   ssl_enforcement     = "Disabled"
-   sku {
-     name     = "MYSQLB50"
-     capacity = 50
-     tier     = "Basic"
-   }
-   administrator_login          = "mysqladmin"
-   administrator_login_password = "Everything-is-bananas-010101"
-   version                      = "5.7"
-   storage_mb                   = "51200"
-   ssl_enforcement              = "Disabled"
- }
+# * Azure MySQL Database
+# Terraform can build any type of infrastructure, not just virtual machines. 
+# Azure offers managed MySQL database servers and a whole host of other 
+# resources. Each resource is documented with all the available settings:
+# https://www.terraform.io/docs/providers/azurerm/r/mysql_server.html
+# Uncomment the code below to add a MySQL server to your resource group.
+# resource "azurerm_mysql_server" "mysql" {
+#   name                = var.mysql_hostname
+#   location            = azurerm_resource_group.tf_azure_guide.location
+#   resource_group_name = azurerm_resource_group.tf_azure_guide.name
+#   ssl_enforcement     = "Disabled"
+#   sku {
+#     name     = "MYSQLB50"
+#     capacity = 50
+#     tier     = "Basic"
+#   }
+#   administrator_login          = "mysqladmin"
+#   administrator_login_password = "Everything-is-bananas-010101"
+#   version                      = "5.7"
+#   storage_mb                   = "51200"
+#   ssl_enforcement              = "Disabled"
+# }
  # This is a sample database that we'll populate with the MySQL sample data
  # set provided here: https://github.com/datacharmer/test_db. With Terraform,
  # everything is Infrastructure as Code. No more manual steps, aging runbooks,
